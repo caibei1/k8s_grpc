f2f09ac968e5fc7a6fd8e912ce48a1461a348f41
caibei1 <1536161955@qq.com>
2020-04-11 14:35:29 +0800

    完成mysql搭建

---
+++ code/yaml/mysql/clean.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect
+
+spawn su root
+expect "密码："
+send "ww0819\r"
+#send "rm -rf /home/wsj/k8s/k8s_grpc/yaml/* \r"
+send "kubectl delete deployment mysql-deployment \r"
+send "kubectl delete service mysql-service \r"
+send "kubectl delete pvc mysql-pv-claim \r"
+send "kubectl delete pv mysql-pv \r"
+send "rm -rf /home/wsj/k8s/Volume/mysql \r"
+
+
+send "kubectl delete configmap mysql-configmap \r"
+send "kubectl delete secret mysql-secret\r"
+send "echo 删除mysql成功...\r"
+
+expect eof
+exit
